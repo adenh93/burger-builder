95c5289019f1088724f08a9124c68f36e9a4911e
Aden Herold <aden.herold1@gmail.com>
2019-11-28 02:05:02 +0000

    fillings array is now flattened using flatMap array method, to determine the size of the array. A default message is displayed if the fillings array is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import BurgerIngredient from "./BurgerIngredient";
 import { IngredientType } from "../../constants/enums";
 
 const Burger = ({ ingredients }) => {
-  const fillings = ingredients.map(({ type, amount }, index) =>
+  const fillings = ingredients.flatMap(({ type, amount }, index) =>
     [...Array(amount).keys()].map(i => (
       <BurgerIngredient key={`${index}_${i}`} type={type} />
     ))
@@ -13,7 +13,7 @@ const Burger = ({ ingredients }) => {
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type={IngredientType.BREADTOP} />
-      {fillings}
+      {fillings.length > 0 ? fillings : <p>Start adding some ingredients!</p>}
       <BurgerIngredient type={IngredientType.BREADBOTTOM} />
     </div>
   );
